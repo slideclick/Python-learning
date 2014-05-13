f402b8fbbee50263cb73a72931b83e5a5955a5bc
Zhou Zhihao <cheerzzh@gmail.com>
2014-05-13 15:54:18 +0800

    oop learning

---
+++ OOP.py
@@ -0,0 +1,14 @@
+
+#OOP
+
+class Student(object):
+	pass
+
+# class: key word to define a class
+# Student: class name
+# object: where it inherits
+
+#a student class instance
+bart  = Student()
+bart
+Student
